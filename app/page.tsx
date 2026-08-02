e3a9ac180eed6a41a6b676bff7f9d625c91ba0c7
loorbach <loorbachpl@gmail.com>
2026-08-02 13:56:25 +0200

    readme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from "react";
 import { PostList } from "../components/post-list";
 
-export default async function Home() {
+export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
